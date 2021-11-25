767c44e9fef8de42d7a6af5f2ee4ae1f8c7c011a
raul <raul@dotts.ro>
2021-11-25 15:15:50 +0200

    finished ts

--- components/meta.tsx
+++ components/meta.tsx
@@ -4,6 +4,7 @@ import { BRAND_NAME } from "../lib/constants";
 const Meta = () => {
   return (
     <Head>
+      <link href={`/prism.css`} rel="stylesheet" />
       <meta name="description" content={`A blog by ${BRAND_NAME}.`} />
       {/* <meta property="og:image" content={HOME_OG_IMAGE_URL} /> */}
     </Head>
